7fb498d218610d465a4cd550bc7ec9545583038c
Wing Lam <winglam2@illinois.edu>
2020-06-18 23:37:08 +0000

    Removing unnecessary files.

--- dt-impact-tracer/lifetime-figure.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Usage: bash lifetime-figure.sh PATH
-
-LOG_FILE="$(basename $1)-log.txt"
-
-mvn package -DskipTests exec:java -Dexec.mainClass="edu.washington.cs.dt.impact.figure.generator.LifetimeFigureGenerator" -Dexec.args="'--lifetime-paths' '$1'" |& tee "$LOG_FILE"
-
